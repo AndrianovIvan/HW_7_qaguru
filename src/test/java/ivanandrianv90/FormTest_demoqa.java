d8793a437c2027e8fa71741c527c17e56445ab42
Ivan Andrianov <ivanandrianv90@gmail.com>
2024-01-02 16:05:13 +0300

    added formtest

---
+++ src/test/java/ivanandrianv90/FormTest_demoqa.java
@@ -0,0 +1,64 @@
+package ivanandrianv90;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+public class FormTest_demoqa {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = false;
+        Configuration.timeout = 5000; // default 4000
+    }
+
+    @Test
+    void fillFormTest() {
+        Selenide.open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue("Peter");
+        $("#lastName").setValue("Ivanov");
+        $("#userEmail").setValue("ivanov_petya@test.com");
+        $("#genterWrapper").find(byText("Male")).click();
+        $("#userNumber").setValue("7123456770");
+        $(".react-datepicker-wrapper").click();
+        $(".react-datepicker__month-select").selectOption("February");
+        $(".react-datepicker__year-select").selectOption("1980");
+        $(".react-datepicker__day--015").click();
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
+        $("#subjectsInput").setValue("Economics").pressEnter();
+        $("#hobbiesWrapper").find(byText("Sports")).click();
+        $("#hobbiesWrapper").find(byText("Music")).click();
+        $("#state").click();
+        $("#stateCity-wrapper").find(byText("Haryana")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").find(byText("Karnal")).click();
+        $("#currentAddress").setValue("Marra avenu, 1");
+        $("#uploadPicture").uploadFromClasspath("test.jpg");
+        $("#submit").click();
+
+
+        $(".modal-content").shouldHave(text("Peter Ivanov"));
+        $(".modal-content").shouldHave(text("ivanov_petya@test.com"));
+        $(".modal-content").shouldHave(text("Male"));
+        $(".modal-content").shouldHave(text("7123456770"));
+        $(".modal-content").shouldHave(text("15 February,1980"));
+        $(".modal-content").shouldHave(text("Computer Science, Economics"));
+        $(".modal-content").shouldHave(text("Sports, Music"));
+        $(".modal-content").shouldHave(text("test.jpg"));
+        $(".modal-content").shouldHave(text("Marra avenu, 1"));
+        $(".modal-content").shouldHave(text("Haryana Karnal"));
+
+    }
+}
